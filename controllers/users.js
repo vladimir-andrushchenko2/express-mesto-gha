0eb7eed52e2105eef51860a0edf2a8bb16b04def
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2022-12-29 21:13:40 +0100

    error handling for user

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 const { update } = require('../models/user');
 const User = require('../models/user');
 const { removeUndefinedEntries } = require('../utils');
+const { NotFound } = require('../errorTypes');
 
 function getUsers(req, res) {
   User.find({})
@@ -10,12 +11,32 @@ function getUsers(req, res) {
 
 function getUser(req, res) {
   User.findById(req.params['userId'])
-    .then(user => res.send({ data: user }))
-    .catch(err => res.status(500).send({ message: err.message }));
+    .then(user => {
+      if (!user) {
+        throw new NotFound("Запрашиваемый пользователь не найден");
+      }
+
+      res.send({ data: user })
+    })
+    .catch(err => {
+      if (err instanceof NotFound) {
+        res.status(404).send({ message: err.message })
+        return;
+      }
+
+      res.status(500).send({ message: err.message })
+    });
 }
 
 function postUser(req, res) {
   const { name, about, avatar } = req.body;
+
+  for (const entry of [name, about, avatar]) {
+    if (!entry) {
+      res.status(400).send({ message: `Заполните все поля для создания пользователя` });
+      return;
+    }
+  }
 
   User.create({ name, about, avatar })
     .then(user => res.send({ data: user }))
@@ -32,6 +53,10 @@ function patchUser(req, res) {
 
   const update = removeUndefinedEntries({ name, about });
 
+  if (!Object.keys(update).length) {
+    res.status(400).send({ message: `Заполните минимум одно поле для обновления пользователя` });
+  }
+
   User.findByIdAndUpdate(req.user._id, update, updateOptions)
     .then(user => res.send({ data: user }))
     .catch(err => res.status(500).send({ message: err.message }));
@@ -44,6 +69,11 @@ function patchUserAvatar(req, res) {
   };
 
   const { avatar } = req.body;
+
+  if (!avatar) {
+    res.status(400).send({ message: `Поле avatar пустое` });
+    return;
+  }
 
   User.findByIdAndUpdate(req.user._id, { avatar }, updateOptions)
     .then(user => res.send({ data: user }))
